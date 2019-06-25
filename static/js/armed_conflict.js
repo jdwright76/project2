694a8565af6fbaf7b133819d7b5686872ce0173d
John Wright <jdwright76@gmail.com>
2019-06-25 18:44:58 -0400

    yah

---
+++ static/js/armed_conflict.js
@@ -0,0 +1,11 @@
+// // Creating map object
+// var myMap = L.map("map", {
+//     center: [0.000000, 0.000000],
+//     zoom: 2.5
+// });;
+
+
+// Grab data with d3
+d3.json("static/js/data.json", function(data) {
+    console.log(data);
+});
